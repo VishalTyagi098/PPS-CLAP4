11856fe2d8b537318692174372e968bd1ba04e92
vishal <vishaltyagi.dev098@gmail.com>
2022-06-21 23:03:13 +0530

    PPS-CLAP4

---
+++ q13.c
@@ -0,0 +1,13 @@
+// Question 13
+
+#include <stdio.h>
+int main()
+{
+    int x=10,y=10;
+    int *p1 =&x,*p2=&y;
+    printf("%d\n",(*p1)++);
+    printf("%d\n",--(*p2));
+    printf("%d\n",(*p1)+(*p2)--);
+    printf("%d\n",++(*p2)-(*p1));
+    return 0;
+}
---
+++ q8.c
@@ -0,0 +1,35 @@
+// Question 8
+
+#include <stdio.h>
+int sum(int a);
+
+int main()
+{
+    int a;
+    printf("Enter the number upto which you wanna find fibonacci series\n");
+    scanf("%d", &a);
+    printf("The %d term of series is \n %d", a,sum(a));
+
+    return 0;
+}
+int sum(int a)
+{
+     int result, n = 1;
+
+    if (a == 1)
+    {
+        result =0;
+        return result;
+    }
+    else if (a == 2)
+    {
+       result =1;
+        return result;
+    }
+    else
+    {
+        result = sum(a - 1) + sum(a - 2);
+       
+    }
+     return result;
+}
